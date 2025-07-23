8e385f20e49be225ed64c04e09b7b733b556ae31
ningmengchongshui <ningmengchongshui@gmail.com>
2025-07-23 21:21:38 +0800

    fix: help

--- src/response/Help/help.ts
+++ src/response/Help/help.ts
@@ -1,5 +1,6 @@
 import { puppeteer } from '@src/api/api'
-import { writeFileSync } from 'fs'
+import { mkdirSync, writeFileSync } from 'fs'
+// import { writeFileSync } from 'fs'
 import md5 from 'md5'
 
 const helpData = {
@@ -9,7 +10,11 @@ const helpData = {
 export async function cache(data, user_id) {
   let tmp = md5(JSON.stringify(data))
   if (helpData.md5 == tmp) return helpData.img
-  writeFileSync('./views/help.json', JSON.stringify(data, null, 2))
+  if (process.env.NODE_ENV === 'development') {
+    const dir = './views/help'
+    mkdirSync(dir, { recursive: true })
+    writeFileSync(`${dir}.json`, JSON.stringify(data, null, 2))
+  }
   helpData.img = await puppeteer.screenshot('help', user_id, data)
   helpData.md5 = tmp
   return helpData.img
