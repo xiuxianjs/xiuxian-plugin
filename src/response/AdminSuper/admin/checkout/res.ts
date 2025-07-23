9c9c5780a775d85c1b8558174fac7e3da69bc591
ningmengchongshui <ningmengchongshui@gmail.com>
2025-07-23 22:57:48 +0800

    update: 修仙更新开启master

--- src/response/AdminSuper/admin/checkout/res.ts
+++ src/response/AdminSuper/admin/checkout/res.ts
@@ -1,21 +1,20 @@
-import { Text, useSend } from 'alemonjs'
 import { exec } from 'child_process'
-
-import { selects } from '@src/response/index'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
-
-export const regular = /^(#|\/)修仙更新/
+import { Text, useSend } from 'alemonjs'
+import { selects } from '@src/response/index'
 
 const currentDir = dirname(fileURLToPath(import.meta.url))
 const mdDir = join(currentDir, '../../../../../')
 
+export const regular = /^(#|\/)修仙更新/
 export default onResponse(selects, e => {
   const Send = useSend(e)
+  if (!e.IsMaster) return
   exec('git  pull', { cwd: mdDir }, function (error, stdout) {
     if (/(Already up[ -]to[ -]date|已经是最新的)/.test(stdout)) {
       Send(Text('目前已经是最新版了~'))
-      return false
+      return
     }
     if (error) {
       Send(
@@ -27,9 +26,8 @@ export default onResponse(selects, e => {
             '\n 请稍后重试。'
         )
       )
-      return false
+      return
     }
     Send(Text('更新成功,请[#重启]'))
   })
-  return false
 })
