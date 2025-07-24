a1a970733f0def496398bfc5a8cca9b9b6cd113a
xyb12345678qwe <xyb12345678qwe@hotmail.com>
2025-07-24 13:27:08 +0800

    update: 优化清除冲水堂代码逻辑

--- src/response/AdminSuper/AdminSuper/Deleteexchange/res.ts
+++ src/response/AdminSuper/AdminSuper/Deleteexchange/res.ts
@@ -10,14 +10,8 @@ export default onResponse(selects, async e => {
     if (!e.IsMaster) return false
 
     Send(Text('开始清除！'))
-    let Exchange
-    try {
-      Exchange = await Read_Exchange()
-    } catch {
-      //没有表要先建立一个！
-      await Write_Exchange([])
-      Exchange = await Read_Exchange()
-    }
+    let Exchange = (await Read_Exchange()) || []
+
     for (let i of Exchange) {
       let usr_qq = i.qq
       let thing = i.name.name
