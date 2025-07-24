ee9d9ab40dc15969e74c1a2de615be21feebec34
youwindy <3193524385@qq.com>
2025-07-24 12:29:00 +0800

    fix: 修复开宗立派

--- src/response/Association/AssociationAdmin/Create_association/res.ts
+++ src/response/Association/AssociationAdmin/Create_association/res.ts
@@ -1,31 +1,16 @@
-import { Text, useSend } from 'alemonjs'
+import { Text, useMessage, useSubscribe } from 'alemonjs'
 
 import { data } from '@src/api/api'
 import { isNotNull, setFileValue, timestampToTime } from '@src/model'
 
 import { selects } from '@src/response/index'
-export const regular = /^(#|＃|\/)?开宗立派/
+export const regular = /^(#|＃|\/)?开宗立派$/
 
 export default onResponse(selects, async e => {
-  const Send = useSend(e)
   let usr_qq = e.UserId
-  const name = e.MessageText.replace(/^(#|＃|\/)?开宗立派/, '')
-  if (!name) {
-    Send(Text('请发送文本,请重新输入:'))
-    return
-  }
-  if (name.length > 6) {
-    Send(Text('宗门名字最多只能设置6个字符,请重新输入'))
-    return
-  }
-  let reg = /[^\u4e00-\u9fa5]/g //汉字检验正则
-  let res = reg.test(name)
-  if (res) {
-    Send(Text('宗门名字只能使用中文,请重新输入'))
-    return
-  }
+  const [message] = useMessage(e)
   let ifexistplay = data.existData('player', usr_qq)
-  if (!ifexistplay) return false
+  if (!ifexistplay) return
   let player = data.getData('player', usr_qq)
   let now_level_id
   now_level_id = data.Level_list.find(
@@ -33,34 +18,75 @@ export default onResponse(selects, async e => {
   ).level_id
 
   if (now_level_id < 22) {
-    Send(Text('修为达到化神再来吧'))
+    e.reply('修为达到化神再来吧')
     return false
   }
   if (isNotNull(player.宗门)) {
-    Send(Text('已经有宗门了'))
+    e.reply('已经有宗门了')
     return false
   }
   if (player.灵石 < 10000) {
-    Send(Text('开宗立派是需要本钱的,攒到一万灵石再来吧'))
+    e.reply('开宗立派是需要本钱的,攒到一万灵石再来吧')
     return false
   }
-  let ifexistass = data.existData('association', name)
-  if (ifexistass) {
-    await Send(Text('该宗门已经存在,请重新输入:'))
-    return false
-  }
-  //await this.reply('功能还在开发中,敬请期待');
-  let now = new Date()
-  let nowTime = now.getTime() //获取当前时间戳
-  let date = timestampToTime(nowTime)
-  player.宗门 = { 宗门名称: name, 职位: '宗主', time: [date, nowTime] }
-  data.setData('player', usr_qq, player)
-  await new_Association(name, usr_qq, e)
-  await setFileValue(usr_qq, -10000, '灵石')
-  await Send(Text('宗门创建成功'))
+  /** 回复 */
+  message.send(
+    format(
+      Text(
+        '请发送宗门名字,一旦设立,无法再改,请慎重取名,(宗门名字最多6个中文字符)'
+      )
+    )
+  )
+  /** 设置上下文 */
+  const [subscribe] = useSubscribe(e, selects)
+  const sub = subscribe.mount(
+    async (event, next) => {
+      let association_name = event.MessageText
+      if (association_name.length > 6) {
+        message.send(format(Text('宗门名字最多只能设置6个字符,请重新输入:')))
+        next()
+      }
+      let reg = /[^\u4e00-\u9fa5]/g //汉字检验正则
+      let res = reg.test(association_name)
+      //res为true表示存在汉字以外的字符
+      if (res) {
+        message.send(format(Text('宗门名字只能使用中文,请重新输入:')))
+        next()
+      }
+      let ifexistass = data.existData('association', association_name)
+      if (ifexistass) {
+        message.send(format(Text('该宗门已经存在,请重新输入:')))
+        next()
+      }
+
+      //await this.reply('功能还在开发中,敬请期待');
+      let now = new Date()
+      let nowTime = now.getTime() //获取当前时间戳
+      let date = timestampToTime(nowTime)
+      let player = data.getData('player', usr_qq)
+      player.宗门 = {
+        宗门名称: association_name,
+        职位: '宗主',
+        time: [date, nowTime]
+      }
+      data.setData('player', usr_qq, player)
+      await new_Association(association_name, usr_qq, e)
+      await setFileValue(usr_qq, -10000, '灵石')
+      // await this.reply('宗门创建成功')
+      message.send(format(Text('宗门创建成功')))
+      /** 结束上下文 */
+      clearTimeout(timeout)
+    },
+    ['UserId']
+  )
+  const timeout = setTimeout(() => {
+    /** 停止上下文 */
+    subscribe.cancel(sub)
+    message.send(format(Text('超时自动取消操作')))
+  }, 30000)
 })
 async function new_Association(name, holder_qq, e) {
-  let usr_qq = e.user_id
+  let usr_qq = e.UserId
   let player = data.getData('player', usr_qq)
   let now_level_id = data.Level_list.find(
     item => item.level_id == player.level_id
