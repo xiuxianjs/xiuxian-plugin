e4d81a05956dc69b9b1b4d15c93e06547f771d58
ningmengchongshui <ningmengchongshui@gmail.com>
2025-07-24 11:19:38 +0800

    fix: 主动消息数据类型错误

--- src/api/api.ts
+++ src/api/api.ts
@@ -17,7 +17,7 @@ export const verc = ({ e }) => {
   return true
 }
 
-export { data, config,  puppeteer }
+export { data, config, puppeteer }
 
 /**
  *
@@ -41,11 +41,14 @@ export async function pushInfo(
   if (isGroup) {
     // 向指定频道发送消息 。SpaceId 从消息中获得，注意这可能不是 ChannelId
     sendToChannel(
-      guild_id,
+      String(guild_id),
       format(typeof msg === 'string' ? Text(msg) : Image(msg))
     )
     return
   }
   // 向指定用户发送消息  OpenID 从消息中获得，注意这可能不是 UserId
-  sendToUser(guild_id, format(typeof msg === 'string' ? Text(msg) : Image(msg)))
+  sendToUser(
+    String(guild_id),
+    format(typeof msg === 'string' ? Text(msg) : Image(msg))
+  )
 }
