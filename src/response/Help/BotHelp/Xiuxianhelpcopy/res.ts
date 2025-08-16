008c41adb4df90b363ecb186e999f6216fd97626
wuleiai <2826874813@qq.com>
2025-08-16 14:43:45 +0800

    .

--- src/response/Help/BotHelp/Xiuxianhelpcopy/res.ts
+++ src/response/Help/BotHelp/Xiuxianhelpcopy/res.ts
@@ -6,7 +6,7 @@ export const regular = /^(#|＃|\/)?修仙扩展(\d+)?$/
 
 export default onResponse(selects, async e => {
   const Send = useSend(e)
-  const helpData = getConfig('help', 'help2')
+  const helpData = await getConfig('help', 'help2')
   // data 是一个数组。需要默认按每3组进行分页。
   // 即可 第一页是 0,1,2 第二页是 3,4,5。注意要处理边界情况。
   const page = e.MessageText.match(/(\d+)/)?.[1]
