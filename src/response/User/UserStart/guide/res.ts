68e856b0029cffe184da52330f48c6039ea3a10c
ningmengchongshui <ningmengchongshui@gmail.com>
2025-09-05 20:51:04 +0800

    fix: 新手引导使用时时事件

--- src/response/User/UserStart/guide/res.ts
+++ src/response/User/UserStart/guide/res.ts
@@ -8,10 +8,16 @@ const res = onResponse(selects, e => {
   const [message] = useMessage(e);
 
   void message.send(format(Text('新手引导开始，可发送‘退出新手引导’退出，发送其他任意内容进入下一步')));
+
   const [subscribe] = useSubscribe(e, selects);
+
   let num = 0;
+
+  //
   const sub = subscribe.mount(
     (event, next) => {
+      const [message] = useMessage(event);
+
       if (event.MessageText === '退出新手引导') {
         void message.send(format(Text('新手引导结束')));
         clearTimeout(timeout);
@@ -29,12 +35,14 @@ const res = onResponse(selects, e => {
     },
     ['UserId']
   );
+
   const timeout = setTimeout(
     () => {
       subscribe.cancel(sub);
       void message.send(format(Text('超时自动取消操作')));
     },
-    1000 * 60 * 30
+    // 5分钟
+    1000 * 60 * 5
   );
 });
 
